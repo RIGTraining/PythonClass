9eb0c6c5196d7347b5a902a51a693b09ce7df3b6
kothet <kothetnaingtun.ict@gmail.com>
2024-12-21 11:36:44 +0630

    setup example

--- PythonCourse/8.OOP Concept/2.self.py
+++ PythonCourse/8.OOP Concept/2.self.py
@@ -2,6 +2,6 @@ class User:
     def __init__(self, name, level):
         self.name = name
         self.level = level
-user = User("ZayarNaing","Admin")
+user = User("KOthet","Admin")
 print(user.name)
 print(user.level)
